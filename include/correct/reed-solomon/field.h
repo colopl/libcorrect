9d415a3aa467b13250722a037aeef3deff3b8a79
Go Kudo <g-kudo@colopl.co.jp>
2025-02-10 08:18:08 +0000

    fix: VisualC compile error

--- include/correct/reed-solomon/field.h
+++ include/correct/reed-solomon/field.h
@@ -66,15 +66,30 @@ static inline void field_destroy(field_t field) {
     free(*(field_element_t **)&field.log);
 }
 
-static inline field_element_t field_add(field_t field __attribute__((unused)), field_element_t l, field_element_t r) {
+static inline field_element_t field_add(field_t field, field_element_t l, field_element_t r) {
+#ifndef _MSC_VER
+    (void)field;
+#else
+    (void)field;
+#endif
     return l ^ r;
 }
 
-static inline field_element_t field_sub(field_t field __attribute__((unused)), field_element_t l, field_element_t r) {
+static inline field_element_t field_sub(field_t field, field_element_t l, field_element_t r) {
+#ifndef _MSC_VER
+    (void)field;
+#else
+    (void)field;
+#endif
     return l ^ r;
 }
 
-static inline field_element_t field_sum(field_t field __attribute__((unused)), field_element_t elem, unsigned int n) {
+static inline field_element_t field_sum(field_t field, field_element_t elem, unsigned int n) {
+#ifndef _MSC_VER
+    (void)field;
+#else
+    (void)field;
+#endif
     // we'll do a closed-form expression of the sum, although we could also
     //   choose to call field_add n times
 
@@ -128,7 +143,12 @@ static inline field_element_t field_div(field_t field, field_element_t l, field_
     return field.exp[res];
 }
 
-static inline field_logarithm_t field_mul_log(field_t field __attribute__((unused)), field_logarithm_t l, field_logarithm_t r) {
+static inline field_logarithm_t field_mul_log(field_t field, field_logarithm_t l, field_logarithm_t r) {
+#ifndef _MSC_VER
+    (void)field;
+#else
+    (void)field;
+#endif
     // this function performs the equivalent of field_mul on two logarithms
     // we save a little time by skipping the lookup step at the beginning
     field_operation_t res = (field_operation_t)l + (field_operation_t)r;
@@ -143,7 +163,12 @@ static inline field_logarithm_t field_mul_log(field_t field __attribute__((unuse
     return (field_logarithm_t)res;
 }
 
-static inline field_logarithm_t field_div_log(field_t field __attribute__((unused)), field_logarithm_t l, field_logarithm_t r) {
+static inline field_logarithm_t field_div_log(field_t field, field_logarithm_t l, field_logarithm_t r) {
+#ifndef _MSC_VER
+    (void)field;
+#else
+    (void)field;
+#endif
     // like field_mul_log, this performs field_div without going through a field_element_t
     field_operation_t res = (field_operation_t)255 + (field_operation_t)l - (field_operation_t)r;
     if (res > 255) {
