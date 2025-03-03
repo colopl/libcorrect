9d9832b0be2cad072fa22a650c1455236474648c
Go Kudo <g-kudo@colopl.co.jp>
2025-03-03 07:24:26 +0000

    fix path

--- container_ci.sh
+++ container_ci.sh
@@ -9,7 +9,7 @@ for COMPILER in "$(which "gcc")" "$(which "clang")"; do
         cd "build"
             cmake .. -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" -DCMAKE_C_COMPILER="${COMPILER}" -DENABLE_LIBCORRECT_TEST=ON
             make -j"$(nproc)" shim test_runners
-            cd "test"
+            cd "tests"
                 ctest -C "${BUILD_TYPE}" --output-on-failure
             cd -
         cd -
